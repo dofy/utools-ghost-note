bedf4a79c6befb04fd0982c7813c86d3ccca6f6d
Seven Yu <dofyyu@gmail.com>
2019-12-24 18:42:20 +0800

    add markdown editor

--- preload.js
+++ preload.js
@@ -1,20 +1,41 @@
 // ! Ghost Note
 
+let mde = new SimpleMDE({
+  element: $('mde'),
+  autofocus: true,
+  spellChecker: false,
+  status: false,
+  toolbar: false,
+})
+
+mde.codemirror.on('change', function() {
+  console.log(mde.value())
+})
+
+function $(id) {
+  return document.getElementById(id)
+}
+
+function autosave(form) {
+  console.log(form.mde)
+}
+
 utools.onPluginEnter(({ code, payload, type }) => {
-  console.log('..:: type', type)
-  console.log('..:: payload', payload)
-  console.log('..:: code', code)
   switch (code) {
     case 'addnote':
+      $('list').style.display = 'none'
+      $('editor').style.display = 'block'
       let _id = payload.appPath
-      let data = utools.db.get(_id) || { _id, note: payload.app }
-      console.log('..:: data', data)
-      utools.db.put(data)
+      let note = utools.db.get(_id) || { _id, note: `# Note for payload.app\n` }
+      console.log('..:: note', note)
+      utools.db.put(note)
+      mde.value(note.note)
       break
     case 'listnote':
     default:
+      $('editor').style.display = 'none'
+      $('list').style.display = 'block'
       let notes = utools.db.allDocs()
-      console.log('..:: notes', notes)
       break
   }
 })
