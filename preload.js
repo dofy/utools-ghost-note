9d21c3687eba8e6735d68f463ced3210e29a8752
Seven Yu <dofyyu@gmail.com>
2019-12-27 19:13:28 +0800

    增加列表页，修改编辑逻辑

--- preload.js
+++ preload.js
@@ -1,41 +1,22 @@
-// ! Ghost Note
+// * Ghost Note
+const { spawn } = require('child_process')
 
-let mde = new SimpleMDE({
-  element: $('mde'),
-  autofocus: true,
-  spellChecker: false,
-  status: false,
-  toolbar: false,
-})
-
-mde.codemirror.on('change', function() {
-  console.log(mde.value())
-})
-
-function $(id) {
-  return document.getElementById(id)
-}
-
-function autosave(form) {
-  console.log(form.mde)
-}
+window.spawn = spawn
 
 utools.onPluginEnter(({ code, payload, type }) => {
+  utools.setExpendHeight(630)
+  updateNotes()
   switch (code) {
     case 'addnote':
-      $('list').style.display = 'none'
-      $('editor').style.display = 'block'
-      let _id = payload.appPath
-      let note = utools.db.get(_id) || { _id, note: `# Note for payload.app\n` }
-      console.log('..:: note', note)
-      utools.db.put(note)
-      mde.value(note.note)
+      editNote(payload.appPath, payload)
       break
     case 'listnote':
     default:
-      $('editor').style.display = 'none'
-      $('list').style.display = 'block'
-      let notes = utools.db.allDocs()
+      editor.close()
       break
   }
+})
+
+utools.onPluginOut(() => {
+  editor.close()
 })
