7129e2e190af1834204bdaf76ac2e806ffb9f350
Seven Yu <dofyyu@gmail.com>
2023-07-17 22:35:41 +0800

    update bootstrap & vue & image

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,77 @@
 // * Ghost Note
+var app = new Vue({
+  el: "#app",
+  data() {
+    // const option = utools.getFeatures(["bind"])[0];
+    return {
+      notes: [],
+      fields: [
+        "#",
+        {
+          key: "app",
+          label: "Bind App",
+          sortable: true,
+        },
+        {
+          key: "created",
+          label: "Created",
+          sortable: true,
+          formatter: "timeFormatter",
+        },
+        {
+          key: "updated",
+          label: "Updated",
+          sortable: true,
+          formatter: "timeFormatter",
+        },
+        "Actions",
+      ],
+      apps: [],
+      status: null,
+      noteid: null,
+    };
+  },
+  watch: {
+    apps(apps) {
+      utools.setFeature({
+        code: "bind",
+        explain: "为当前应用添加 Ghost Note",
+        cmds: [
+          {
+            type: "window",
+            label: "Bind Ghost Note",
+            match: {
+              app: apps,
+            },
+          },
+        ],
+      });
+      utools.showNotification("应用列表保存成功!");
+    },
+  },
+  methods: {
+    runApp(path) {
+      utools.shellOpenPath(path);
+    },
+    timeFormatter(value) {
+      const date = new Date(value);
+      return [
+        [date.getFullYear(), date.getMonth() + 1, date.getDate()].join("/"),
+        [date.getHours(), date.getMinutes(), date.getSeconds()]
+          .map((item) => (item > 9 ? item : "0" + item))
+          .join(":"),
+      ].join(" ");
+    },
+  },
+});
 
-var timeid = null;
-var mde = new SimpleMDE({
+let timeid = null;
+let mde = new SimpleMDE({
   autofocus: true,
   status: false,
   toolbar: false,
   spellChecker: false,
+  autoDownloadFontAwesome: true,
   shortcuts: {
     toggleBlockquote: null,
     toggleBold: null,
@@ -32,8 +98,8 @@ mde.codemirror.on("change", (editor, evt) => {
     saveNote();
   }, 1000);
 });
+
 mde.codemirror.on("keydown", (editor, evt) => {
-  console.log(evt);
   if (evt.ctrlKey || evt.metaKey) {
     switch (evt.key) {
       case "s":
@@ -49,60 +115,6 @@ mde.codemirror.on("keydown", (editor, evt) => {
   }
 });
 
-var app = new Vue({
-  el: "#app",
-  data() {
-    const option = utools.getFeatures(["bind"])[0];
-    return {
-      notes: [],
-      fields: [
-        "#",
-        {
-          key: "app",
-          label: "Bind App",
-          sortable: true,
-        },
-        {
-          key: "created",
-          label: "Created",
-          sortable: true,
-          formatter: "timeFormatter",
-        },
-        {
-          key: "updated",
-          label: "Updated",
-          sortable: true,
-          formatter: "timeFormatter",
-        },
-        "Actions",
-      ],
-      option,
-      status: null,
-      noteid: null,
-    };
-  },
-  watch: {
-    // status(newVal, oldVal) {
-    //   document.getElementById("editor-box").hidden = newVal !== "bind";
-    //   document.getElementById("option-box").hidden = newVal !== "option";
-    // },
-  },
-  methods: {
-    runApp(path) {
-      utools.shellOpenPath(path);
-    },
-    timeFormatter(value) {
-      const date = new Date(value);
-      return [
-        [date.getFullYear(), date.getMonth() + 1, date.getDate()].join("/"),
-        [date.getHours(), date.getMinutes(), date.getSeconds()]
-          .map((item) => (item > 9 ? item : "0" + item))
-          .join(":"),
-      ].join(" ");
-    },
-  },
-});
-
 function updateNotes() {
   app.notes = utools.db
     .allDocs()
@@ -110,16 +122,11 @@ function updateNotes() {
 }
 
 async function editNote(id, payload) {
-  // const url =
-  // (await utools.readCurrentBrowserUrl()) ||
-  // (await utools.readCurrentFolderPath()) ||
-  // "";
   let note = utools.db.get(id) || {
     _id: id,
     app: payload.app,
     created: new Date(),
     updated: new Date(),
-    // content: `# Note for ${payload.app}\n \`${url}\`\n\n`,
     content: `# Note for ${payload.app}\n`,
   };
   app.status = "bind";
@@ -127,7 +134,6 @@ async function editNote(id, payload) {
   setTimeout(() => {
     mde.value(note.content);
   }, 0);
-
   utools.db.put(note);
 }
 
@@ -140,11 +146,11 @@ function saveNote() {
 
 function delNote(evt) {
   app.$bvModal
-    .msgBoxConfirm("Sure to delete this Note?", {
-      title: "Confirm",
+    .msgBoxConfirm("确定要删除这条 Note 吗?", {
+      title: "确认",
       okVariant: "danger",
-      okTitle: "Delete",
-      cancelTitle: "Cancel",
+      okTitle: "删除",
+      cancelTitle: "取消",
     })
     .then((value) => {
       if (value) {
@@ -154,15 +160,38 @@ function delNote(evt) {
     });
 }
 
+// safeid
 function safeid(id) {
   return id.replace(/\//g, "{{._.}}");
 }
 
+// unsafeid
 function unsafeid(id) {
   return id.replace(/\{\{\._\.\}\}/g, "/");
 }
 
+// init settings
 function initSettings() {
   const features = utools.getFeatures(["bind"]);
-  console.log("bind:", features);
+  if (features.length <= 0) {
+    // fix old user's options by database
+    app.apps = utools.db.allDocs().map((item) => item.app);
+  } else {
+    // read apps from features
+    app.apps = features[0].cmds[0].match.app;
+  }
+
+  utools.setFeature({
+    code: "bind",
+    explain: "为当前应用添加 Ghost Note",
+    cmds: [
+      {
+        type: "window",
+        label: "Bind Ghost Note",
+        match: {
+          app: app.apps,
+        },
+      },
+    ],
+  });
 }
