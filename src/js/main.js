e0fb459c6a55cf16bec4d02825369161098e2a61
Seven Yu <dofyyu@gmail.com>
2023-07-17 15:53:56 +0800

    update bootstrap & vue

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // * Ghost Note
 
-var timeid = null
+var timeid = null;
 var mde = new SimpleMDE({
   autofocus: true,
   status: false,
@@ -22,133 +22,147 @@ var mde = new SimpleMDE({
     toggleSideBySide: null,
     toggleFullScreen: null,
   },
-})
+});
 
-mde.codemirror.on('change', (editor, evt) => {
+mde.codemirror.on("change", (editor, evt) => {
   // save note
-  timeid && clearTimeout(timeid)
+  timeid && clearTimeout(timeid);
   timeid = setTimeout(() => {
-    timeid = null
-    saveNote()
-  }, 1000)
-})
-mde.codemirror.on('keydown', (editor, evt) => {
+    timeid = null;
+    saveNote();
+  }, 1000);
+});
+mde.codemirror.on("keydown", (editor, evt) => {
+  console.log(evt);
   if (evt.ctrlKey || evt.metaKey) {
     switch (evt.key) {
-      case 's':
-        break
-      case 'w':
-        app.status = 'ghost'
-        saveNote()
-        updateNotes()
-        break
+      case "s":
+        break;
+      case "w":
+        app.status = "ghost";
+        saveNote();
+        updateNotes();
+        break;
       default:
-        break
+        break;
     }
   }
-})
+});
 
 var app = new Vue({
-  el: '#app',
+  el: "#app",
   data() {
+    const option = utools.getFeatures(["bind"])[0];
     return {
       notes: [],
       fields: [
-        '#',
+        "#",
         {
-          key: 'app',
-          label: 'Bind App',
+          key: "app",
+          label: "Bind App",
           sortable: true,
         },
         {
-          key: 'created',
-          label: 'Created',
+          key: "created",
+          label: "Created",
           sortable: true,
-          formatter: 'timeFormatter',
+          formatter: "timeFormatter",
         },
         {
-          key: 'updated',
-          label: 'Updated',
+          key: "updated",
+          label: "Updated",
           sortable: true,
-          formatter: 'timeFormatter',
+          formatter: "timeFormatter",
         },
-        'Actions',
+        "Actions",
       ],
+      option,
       status: null,
       noteid: null,
-    }
+    };
   },
   watch: {
-    status(newVal, oldVal) {
-      document.getElementById('editorbox').hidden = newVal !== 'bind'
-    },
+    // status(newVal, oldVal) {
+    //   document.getElementById("editor-box").hidden = newVal !== "bind";
+    //   document.getElementById("option-box").hidden = newVal !== "option";
+    // },
   },
   methods: {
     runApp(path) {
-      utools.shellOpenPath(path)
+      utools.shellOpenPath(path);
     },
     timeFormatter(value) {
-      const date = new Date(value)
+      const date = new Date(value);
       return [
-        [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('/'),
+        [date.getFullYear(), date.getMonth() + 1, date.getDate()].join("/"),
         [date.getHours(), date.getMinutes(), date.getSeconds()]
-          .map((item) => (item > 9 ? item : '0' + item))
-          .join(':'),
-      ].join(' ')
+          .map((item) => (item > 9 ? item : "0" + item))
+          .join(":"),
+      ].join(" ");
     },
   },
-})
+});
 
 function updateNotes() {
   app.notes = utools.db
     .allDocs()
-    .sort((x, y) => +new Date(y.updated) - +new Date(x.updated))
+    .sort((x, y) => +new Date(y.updated) - +new Date(x.updated));
 }
 
-function editNote(id, payload) {
+async function editNote(id, payload) {
+  // const url =
+  // (await utools.readCurrentBrowserUrl()) ||
+  // (await utools.readCurrentFolderPath()) ||
+  // "";
   let note = utools.db.get(id) || {
     _id: id,
     app: payload.app,
     created: new Date(),
     updated: new Date(),
+    // content: `# Note for ${payload.app}\n \`${url}\`\n\n`,
     content: `# Note for ${payload.app}\n`,
-  }
-  app.status = 'bind'
-  app.noteid = id
+  };
+  app.status = "bind";
+  app.noteid = id;
   setTimeout(() => {
-    mde.value(note.content)
-  }, 0)
+    mde.value(note.content);
+  }, 0);
 
-  utools.db.put(note)
+  utools.db.put(note);
 }
 
 function saveNote() {
-  let note = utools.db.get(app.noteid)
-  note.content = mde.value()
-  note.updated = new Date()
-  utools.db.put(note)
+  let note = utools.db.get(app.noteid);
+  note.content = mde.value();
+  note.updated = new Date();
+  utools.db.put(note);
 }
 
 function delNote(evt) {
   app.$bvModal
-    .msgBoxConfirm('Sure to delete this Note?', {
-      title: 'Confirm',
-      okVariant: 'danger',
-      okTitle: 'Delete',
-      cancelTitle: 'Cancel',
+    .msgBoxConfirm("Sure to delete this Note?", {
+      title: "Confirm",
+      okVariant: "danger",
+      okTitle: "Delete",
+      cancelTitle: "Cancel",
     })
     .then((value) => {
       if (value) {
-        utools.db.remove(evt.target.value)
-        updateNotes()
+        utools.db.remove(evt.target.value);
+        updateNotes();
       }
-    })
+    });
 }
 
 function safeid(id) {
-  return id.replace(/\//g, '{{._.}}')
+  return id.replace(/\//g, "{{._.}}");
 }
 
 function unsafeid(id) {
-  return id.replace(/\{\{\._\.\}\}/g, '/')
+  return id.replace(/\{\{\._\.\}\}/g, "/");
+}
+
+function initSettings() {
+  const features = utools.getFeatures(["bind"]);
+  console.log("bind:", features);
 }
