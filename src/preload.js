696705c987681956e669076b80c4658ac0f1a631
Seven Yu <dofyyu@gmail.com>
2023-07-17 22:53:27 +0800

    fix bugs for uTools v3.3.0

--- src/preload.js
+++ src/preload.js
@@ -1,16 +1,19 @@
 // * Ghost Note
 utools.onPluginEnter(({ code, payload }) => {
-  utools.setExpendHeight(630)
-  app.status = code
-  updateNotes()
+  utools.setExpendHeight(630);
+  app.status = code;
+  updateNotes();
   switch (code) {
-    case 'bind':
-      editNote(payload.appPath, payload)
-      break
-    case 'ghost':
+    case "bind":
+      editNote(payload.appPath, payload);
+      break;
+    case "option":
+      initSettings();
+      break;
+    case "ghost":
     default:
-      break
+      break;
   }
-})
+});
 
-utools.onPluginOut(() => {})
+utools.onPluginOut(() => {});
